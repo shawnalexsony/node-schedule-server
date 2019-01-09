0b58d547c801585906c4466252a860ac4cc5f349
Shawn Alex Sony <shawnalex97@gmail.com>
2019-01-09 15:42:06 +0530

    updated

--- schedule.js
+++ schedule.js
@@ -3,12 +3,7 @@ var schedule = require('node-schedule');
 var rule = new schedule.RecurrenceRule();
 // rule.second = 5;
 rule.second = new schedule.Range(0, 59, 5);
-// schedule.scheduleJob(rule, function(){
-//     // console.log(new Date(), 'The 5th second of the minute.');
-//    console.log('every 5 seconds');
-
-// });
-
+// Set up the time or date for scheduled tasks
 
 
 const express = require('express');
@@ -29,6 +24,7 @@ app.listen(8080, () => {
   schedule.scheduleJob(rule, function(){
     // console.log(new Date(), 'The 5th second of the minute.');
    console.log('every 5 seconds');
+    //tasks to be done
 
 });
 
