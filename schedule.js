2b0f3d3118e590cd9972c59c089fbc5041421371
Shawn Alex Sony <shawnalex97@gmail.com>
2019-01-09 15:38:01 +0530

    add files

---
+++ schedule.js
@@ -0,0 +1,35 @@
+var schedule = require('node-schedule');
+
+var rule = new schedule.RecurrenceRule();
+// rule.second = 5;
+rule.second = new schedule.Range(0, 59, 5);
+// schedule.scheduleJob(rule, function(){
+//     // console.log(new Date(), 'The 5th second of the minute.');
+//    console.log('every 5 seconds');
+
+// });
+
+
+
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Express!</h1>');
+  console.log('req from browser');
+});
+
+
+app.listen(8080, () => {
+  console.log('Server is up on port 8080');
+
+  schedule.scheduleJob(rule, function(){
+    // console.log(new Date(), 'The 5th second of the minute.');
+   console.log('every 5 seconds');
+
+});
+
+});
